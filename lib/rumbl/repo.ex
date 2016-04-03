d64cc72258844c553dcbf2c8b750c44a00679aac
Nithin Bekal <nithinbekal@gmail.com>
2016-04-03 16:31:52 +0530

    User creation form

--- lib/rumbl/repo.ex
+++ lib/rumbl/repo.ex
@@ -1,27 +1,3 @@
 defmodule Rumbl.Repo do
-  @moduledoc """
-  In memory repository.
-  """
-
-  def all(Rumbl.User) do
-    [
-      %Rumbl.User{id: "1", name: "Jose", username: "josevalim", password: "elixir"},
-      %Rumbl.User{id: "2", name: "Bruce", username: "redrapids", password: "7langs"},
-      %Rumbl.User{id: "3", name: "Jose", username: "chrismccord", password: "phx"}
-    ]
-  end
-
-  def all(_module), do: []
-
-  def get(module, id) do
-    all(module)
-    |> Enum.find(fn map -> map.id == id end)
-  end
-
-  def get_by(module, params) do
-    all(module)
-    |> Enum.find(fn map ->
-         Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
-    end)
-  end
+  use Ecto.Repo, otp_app: :rumbl
 end
