26cc4fe8318c1f98204a534dd2fa64e4d885e33a
Nithin Bekal <nithinbekal@gmail.com>
2016-04-03 12:15:45 +0530

    Create User index and show actions

---
+++ web/controllers/user_controller.ex
@@ -0,0 +1,16 @@
+defmodule Rumbl.UserController do
+  use Rumbl.Web, :controller
+
+  alias Rumbl.Repo
+  alias Rumbl.User
+
+  def index(conn, _params) do
+    users = Repo.all(User)
+    render conn, "index.html", users: users
+  end
+
+  def show(conn, %{"id" => id}) do
+    user = Repo.get(User, id)
+    render conn, "show.html", user: user
+  end
+end
