1308c594b270598aa2b76fcd62901137243da044
Nithin Bekal <nithinbekal@gmail.com>
2016-04-05 23:28:23 +0530

    Add slugs to Video

--- test/controllers/video_controller_test.exs
+++ test/controllers/video_controller_test.exs
@@ -1,7 +1,6 @@
 defmodule Rumbl.VideoControllerTest do
   use Rumbl.ConnCase
 
-  alias Rumbl.Video
   @valid_attrs %{description: "some content", title: "some content", url: "some content"}
   @invalid_attrs %{}
 
