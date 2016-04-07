176f83ad83b923d8e8a6dd4ae5f33b911357ba3c
Nithin Bekal <nithinbekal@gmail.com>
2016-04-07 19:37:31 +0530

    Share wolfram results in the channels

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,8 +12,12 @@
 
 alias Rumbl.Repo
 alias Rumbl.Category
+alias Rumbl.User
 
 for category <- ~w(Action Drama Romance Comedy Sci-fi) do
   Repo.get_by(Category, name: category) ||
     Repo.insert!(%Category{name: category})
 end
+
+Repo.insert!(%User{name: "Wolfram", username: "wolfram"})
+
